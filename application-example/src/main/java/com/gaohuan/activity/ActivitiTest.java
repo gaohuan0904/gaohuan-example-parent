06cfd6809f246e8d4df7968d4ecf46a62d61b849
huan <gaohuan0904@163.com>
2016-02-28 23:21:13 +0800

    新的测试代码

---
+++ application-example/src/main/java/com/gaohuan/activity/ActivitiTest.java
@@ -0,0 +1,13 @@
+package com.gaohuan.activity;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+
+/**
+ * Created by huan on 2016/2/28.
+ */
+public class ActivitiTest {
+    public static void main(String[] args) {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+    }
+}
