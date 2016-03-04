c350098672eb359aebfa375913ac95c38bcd18a5
616994589 <616994589@qq.com>
2016-03-04 18:24:55 +0800

    SpringJpa+Jpa+hibernate

--- gaohuan-concurrent-biz/src/main/java/com/gaohuan/biz/Main.java
+++ gaohuan-concurrent-biz/src/main/java/com/gaohuan/biz/Main.java
@@ -1,22 +1,26 @@
 package com.gaohuan.biz;
 
-import com.gaohuan.biz.entity.User;
-import com.gaohuan.biz.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.net.MalformedURLException;
 
 /**
  * Created by gh on 2016/3/4 0004.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        /*
         UserService userService = (UserService) context.getBean("userService");
         User user = new User();
         user.setAge(25);
         user.setName("gaohuan");
         userService.save(user);
+        */
+        Example example = (Example) context.getBean("example");
+        example.addLink("高欢", "www.baidu.com");
         System.out.println("成功");
         System.exit(1);
     }
