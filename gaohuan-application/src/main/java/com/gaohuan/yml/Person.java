e8d6dcf12b668603fe29f8c7b43b8ceadf4185f0
gaohuan0904 <gaohuan0904@163.com>
2016-05-16 13:39:29 +0800

    提交代码

--- gaohuan-application/src/main/java/com/gaohuan/yml/Person.java
+++ gaohuan-application/src/main/java/com/gaohuan/yml/Person.java
@@ -4,4 +4,41 @@ package com.gaohuan.yml;
  * Created by gh on 2016/5/16 0016.
  */
 public class Person {
+
+    private String name;
+    private int age;
+    private Person spouse;
+    private Person[] children;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person getSpouse() {
+        return spouse;
+    }
+
+    public void setSpouse(Person spouse) {
+        this.spouse = spouse;
+    }
+
+    public Person[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(Person[] children) {
+        this.children = children;
+    }
 }
